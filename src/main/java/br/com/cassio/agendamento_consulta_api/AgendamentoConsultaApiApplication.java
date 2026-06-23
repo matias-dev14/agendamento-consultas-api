207a0cd239039f4b390dbb3a9c3e62453f40ac99
matias-dev14 <matias.dev.14@gmail.com>
2026-06-23 00:04:41 -0300

    chore: cria projeto Spring Boot inicial

---
+++ src/main/java/br/com/cassio/agendamento_consulta_api/AgendamentoConsultaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cassio.agendamento_consulta_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoConsultaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoConsultaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cassio/agendamento_consulta_api/AgendamentoConsultaApiApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.cassio.agendamento_consulta_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class AgendamentoConsultaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
